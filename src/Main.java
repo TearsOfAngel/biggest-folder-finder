e6e678932ec829ef4b3cd76a8329cdee871bee7b
vcarstein <yurij.yashenev.00@gmail.com>
2023-06-24 12:32:48 +0300

    Added: method getFolderSize()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        String folderPath = "/Users/vcarstein/Desktop/projects";
+        File file = new File(folderPath);
 
+        System.out.println(getFolderSize(file));
+
+
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
