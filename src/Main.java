65cbea55b32ff859c99bd35ab8b0b9657b8164d4
vcarstein <yurij.yashenev.00@gmail.com>
2023-06-25 11:58:13 +0300

    Added: getHumanReadableSize getSizeFromHumanReadable

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
+    private static final char[] sizeMultipliers = {
+            'B', 'K', 'M', 'G', 'T'
+    };
+    private static final String[] sizeNames =
+            {"b", "Kb", "Mb", "Gb", "Tb"};
     public static void main(String[] args) {
-
-        String folderPath = "/Users/vcarstein/Desktop/";
+        String folderPath = "/Users/vcarstein/Desktop/projects";
         File file = new File(folderPath);
 
         long start = System.currentTimeMillis();
@@ -16,22 +22,46 @@ public class Main {
         long size = pool.invoke(calculator);
         System.out.println(size);
 
+        System.out.println(getHumanReadableSize(size));
+        System.out.println(getSizeFromHumanReadable(getHumanReadableSize(size)));
+
         long duration = System.currentTimeMillis() - start;
+
+
         System.out.println(duration);
 
-
+    }
+    //24B, 243Kb, 36Mb, 34Gb, 42Tb
+    public static String getHumanReadableSize(long size) {
+        for (int i = 0; i <sizeMultipliers.length; i++) {
+            double value = size / Math.pow(1024, i);
+            if (value < 1024) {
+                return Math.round(value) +
+                        String.valueOf(sizeMultipliers[i]) +
+                        (i > 0 ? "b" : "");
+            }
+        }
+        return "bigger";
     }
 
-    public static long getFolderSize(File folder) {
-        if (folder.isFile()) {
-            return folder.length();
-        }
+    public static long getSizeFromHumanReadable(String size) {
+        HashMap<Character, Integer> char2multiplier = getMultipliers();
+        char sizeFactor = size
+                .replaceAll("[0-9\\s+]+", "")
+                .charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length = multiplier * Long.parseLong(
+                size.replaceAll("[^0-9]", ""));
+        return length;
+    }
 
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            sum += getFolderSize(file);
+    private static HashMap<Character, Integer> getMultipliers() {
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            char2multiplier.put(
+                    sizeMultipliers[i],
+                    (int) Math.pow(1024, i));
         }
-        return sum;
+        return char2multiplier;
     }
 }
