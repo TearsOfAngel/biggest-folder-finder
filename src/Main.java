17f1c42f69d163f71971936bd45e6591e6846e37
vcarstein <yurij.yashenev.00@gmail.com>
2023-06-26 20:12:11 +0300

    Added: Nodes now have levels The tree of files and folders can now be displayed

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,28 @@
 import java.io.File;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    private static final char[] sizeMultipliers = {
-            'B', 'K', 'M', 'G', 'T'
-    };
-    private static final String[] sizeNames =
-            {"b", "Kb", "Mb", "Gb", "Tb"};
+
     public static void main(String[] args) {
-        String folderPath = "/Users/vcarstein/Desktop/projects";
+
+        ParametersBag bag = new ParametersBag(args);
+
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
         File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
 
         long start = System.currentTimeMillis();
 
-        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
-
-        System.out.println(getHumanReadableSize(size));
-        System.out.println(getSizeFromHumanReadable(getHumanReadableSize(size)));
+        pool.invoke(calculator);
+        System.out.println(root);
 
         long duration = System.currentTimeMillis() - start;
 
 
-        System.out.println(duration);
+        System.out.println(duration + " ms");
 
-    }
-    //24B, 243Kb, 36Mb, 34Gb, 42Tb
-    public static String getHumanReadableSize(long size) {
-        for (int i = 0; i <sizeMultipliers.length; i++) {
-            double value = size / Math.pow(1024, i);
-            if (value < 1024) {
-                return Math.round(value) +
-                        String.valueOf(sizeMultipliers[i]) +
-                        (i > 0 ? "b" : "");
-            }
-        }
-        return "bigger";
-    }
-
-    public static long getSizeFromHumanReadable(String size) {
-        HashMap<Character, Integer> char2multiplier = getMultipliers();
-        char sizeFactor = size
-                .replaceAll("[0-9\\s+]+", "")
-                .charAt(0);
-        int multiplier = char2multiplier.get(sizeFactor);
-        long length = multiplier * Long.parseLong(
-                size.replaceAll("[^0-9]", ""));
-        return length;
-    }
-
-    private static HashMap<Character, Integer> getMultipliers() {
-        HashMap<Character, Integer> char2multiplier = new HashMap<>();
-        for (int i = 0; i < sizeMultipliers.length; i++) {
-            char2multiplier.put(
-                    sizeMultipliers[i],
-                    (int) Math.pow(1024, i));
-        }
-        return char2multiplier;
     }
 }
---
+++ src/ParametersBag.java
@@ -0,0 +1,14 @@
+public class ParametersBag {
+    //TODO realize ParametersBag Class
+    public ParametersBag(String args[]) {
+
+    }
+
+    public long getLimit() {
+        return 0;
+    }
+
+    public String getPath() {
+        return "";
+    }
+}
